ac2a3d19a679f48e2ca046f0b6a721871fc705b4
Alexander Kondratev <kondratev.av@inbox.ru>
2023-04-15 15:51:49 +0500

    docker prune script

---
+++ docker-cleaner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Docker cleaner: Start removing all unused containers, networks, images."
+
+CURRENT_MOUNTH=$(date "+%Y-%m")
+LOG_PATH="../logs/docker/$CURRENT_MOUNTH-docker_prune.log"
+
+DOCKER_PRUNE=$(docker system prune -a -f)
+LOG_INFO="$(date "+%Y-%m-%d %H:%M:%S") $DOCKER_PRUNE"
+
+
+if [ ! -d "$(dirname "$LOG_PATH")" ]; then
+  mkdir -p "$(dirname "$LOG_PATH")"
+fi
+
+if [ ! -f "$LOG_PATH" ]; then
+  touch "$LOG_PATH"
+fi
+
+echo "$LOG_INFO" >> "$LOG_PATH" 2>&1
+echo "Success: $LOG_INFO"
+
+# @daily /home/zugzwang/bash_srcipts/docker-cleaner.sh
